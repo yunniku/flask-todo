911a041d0b75c70c4b6a1c46ebca01c46d724c23
박윤희 <bag-yunhui@bag-yunhuiui-MacBookAir.local>
2026-05-21 13:46:35 +0900

    SQLite로 변경

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-# config.py
 import os
 
 class Config:
     SECRET_KEY = 'my-super-secret-key-change-this-later'
     
-    # MySQL 연결 설정
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:4050@localhost/flask_todo'
+    # SQLite로 변경!
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'todo.db')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_COOKIE_SECURE = False  # 개발환경
+    SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
